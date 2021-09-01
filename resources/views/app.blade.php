b56e703a1c0937d2cb82e17939b7211513bb73bb
Al'tair <neogil28@gmail.com>
2021-09-01 18:59:21 +0300

    The first commit with loading frameworks
    Two frameworks have been added. Laravel backend and angular frontend.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html><html lang="en"><head>
+    <meta charset="utf-8">
+    <title>AppSchNk</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="/assets/AppSchNk/styles.31d6cfe0d16ae931b73c.css"></head>
+<body>
+<app-root></app-root>
+<script src="/assets/AppSchNk/runtime.8e3e3957547951ec68b7.js" defer></script><script src="/assets/AppSchNk/polyfills.68ab0b6520c9a57432fc.js" defer></script><script src="/assets/AppSchNk/main.0aed24552476087a811f.js" defer></script>
+
+</body></html>
