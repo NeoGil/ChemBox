4ce500b4fd91d72b57ac6ba17d33646b064331a8
Al'tairs <neogil28@gmail.com>
2021-09-26 22:55:36 +0300

    The preloader component has been updated in the front application

--- resources/frontend/AppSchNk/src/app/components/layout/preloader/preloader.component.ts
+++ resources/frontend/AppSchNk/src/app/components/layout/preloader/preloader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PreloaderService} from "../../../services/preloaders/preloader.service";
 
 @Component({
   selector: 'app-preloader',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreloaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public preloaderService: PreloaderService
+  ) { }
 
   ngOnInit(): void {
   }
