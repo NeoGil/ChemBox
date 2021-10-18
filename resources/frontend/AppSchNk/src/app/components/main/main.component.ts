000048c9983324b53d1d19c4e6577a351a69ea39
Al'tairs <neogil28@gmail.com>
2021-10-18 21:30:31 +0300

    Promo and second sections have been added to the main component

--- resources/frontend/AppSchNk/src/app/components/main/main.component.ts
+++ resources/frontend/AppSchNk/src/app/components/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Courses} from "../../Models/courses";
+import {CoursesService} from "../../services/courses.service";
+import {errorObject} from "rxjs/internal-compatibility";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  courses: Courses[];
+
+  constructor(
+    private coursesService: CoursesService
+  ) { }
 
   ngOnInit(): void {
+    this.coursesService.getCourses().subscribe((data) => {
+
+      let b: Courses[] = [];
+      var arr = [];
+
+      for (var i = 0; i < data.length; i++)
+        arr.push(i);
+      this.shuffle(arr);
+
+      for (let i = 0; i < 4; i++) {
+        b.push(data[arr[i]])
+      }
+
+      this.courses = b;
+    })
   }
 
+  shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
 }
